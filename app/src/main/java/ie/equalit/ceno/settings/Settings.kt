3062a818d9e19e70dbc52da871d9b21b757000ef
sphalt2 <sai@equalitie.org>
2025-07-14 15:49:09 -0400

    Revert "Disable metrics by default, hide settings option"
    This reverts commit 39260e4ae2727e1dfe14cc232647f6d04908d520.

--- app/src/main/java/ie/equalit/ceno/settings/Settings.kt
+++ app/src/main/java/ie/equalit/ceno/settings/Settings.kt
@@ -367,13 +367,11 @@ object Settings {
     }
 
     fun isOuinetMetricsEnabled(context: Context) : Boolean {
-        /*
         if (context.isFirstInstall()) {
             return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(
                 context.getString(R.string.pref_key_metrics_ouinet), true
             )
         }
-        */
         return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(
             context.getString(R.string.pref_key_metrics_ouinet), false
         )
