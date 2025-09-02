b6d1d34de42da3bc8976b6e11bdfcfef6b418b3e
sphalt2 <sai@equalitie.org>
2025-09-02 10:53:23 -0400

    Fix UI error for personal homepage

--- app/src/main/java/ie/equalit/ceno/home/HomeFragment.kt
+++ app/src/main/java/ie/equalit/ceno/home/HomeFragment.kt
@@ -266,11 +266,13 @@ class HomeFragment : BaseHomeFragment() {
                     ContextCompat.getDrawable(it, R.color.fx_mobile_private_layer_color_3)
                 binding.sessionControlRecyclerView.background =
                     ContextCompat.getDrawable(it, R.color.fx_mobile_private_layer_color_3)
+                binding.wordmark.drawable.setTint(ContextCompat.getColor(it, R.color.photonWhite))
             } else {
                 binding.homeAppBar.background =
                     ContextCompat.getDrawable(it, R.color.ceno_home_background)
                 binding.sessionControlRecyclerView.background =
                     ContextCompat.getDrawable(it, R.color.ceno_home_background)
+                binding.wordmark.drawable.setTintList(null)
             }
         }
         applyTheme()
