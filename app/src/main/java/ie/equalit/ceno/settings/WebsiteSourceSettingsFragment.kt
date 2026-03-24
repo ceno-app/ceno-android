9bd6cd85ff1b710046625d6000acf6ce4a0d4504
sphalt2 <sai@equalitie.org>
2026-03-24 10:16:39 -0400

    Adjusts conditions for showing warning on website sources settings page

--- app/src/main/java/ie/equalit/ceno/settings/WebsiteSourceSettingsFragment.kt
+++ app/src/main/java/ie/equalit/ceno/settings/WebsiteSourceSettingsFragment.kt
@@ -124,13 +124,13 @@ class WebsiteSourceSettingsFragment : PreferenceFragmentCompat() {
             isWarningVisible= true
             warningMessage = getString(R.string.warning_all_website_sources_disabled)
         }
-        if (!public && !origin && !shared && private) {
+        if (!public && !origin && !shared) {
             //if all public sources are disabled
             isWarningVisible = true
             warningMessage = getString(R.string.warning_public_website_sources_disabled)
         }
         //if private source is disabled
-        if(!private && origin && public && shared) {
+        if(!private) {
             isWarningVisible = true
             warningMessage = getString(R.string.warning_personal_website_sources_disabled)
         }
